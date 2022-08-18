068402069406b3aeb8c99f27d8707f29629c45cb
rosalievan <r.vanonzenoort98@gmail.com>
2022-08-18 15:52:58 +0100

    abbreviating items

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -22,38 +22,35 @@ export class GildedRose {
         let irregularitems: Array<String> = ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros']
 
         for (let i = 0; i < this.items.length; i++) {
-            let name: String = this.items[i].name;
-            let sellIn: Number = this.items[i].sellIn;
-            let quality: Number = this.items[i].quality;
-
+            let item = this.items[i]
 
             if (
-                !(irregularitems.includes(name)) && quality > 0) {
-                    this.items[i].quality = this.items[i].quality - 1} 
+                !(irregularitems.includes(item.name)) && item.quality > 0) {
+                    item.quality -=  1} 
                 
                 else {
-                    if (quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
+                    if (item.quality < 50) {
+                        item.quality += 1
 
-                        if (name == 'Backstage passes to a TAFKAL80ETC concert' && quality< 50){
-                            let addition : number = this.items[i].sellIn<6 ? 2 : this.items[i].sellIn < 11 ?  1: 0
-                            this.items[i].quality = this.items[i].quality + addition
+                        if (item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.quality< 50){
+                            let addition : number = item.sellIn<6 ? 2 : item.sellIn < 11 ?  1: 0
+                            item.quality += addition
                         }
                     }
                 }
 
-            if (name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
+            if (item.name != 'Sulfuras, Hand of Ragnaros') {
+                item.sellIn -= 1;
             }
 
-            if (this.items[i].sellIn < 0) {
+            if (item.sellIn < 0) {
 
-                if (!(irregularitems.includes(name)) && quality > 0 ){
-                    this.items[i].quality = this.items[i].quality - 1 }
-                else if (!(name == "Aged Brie")){
-                    this.items[i].quality = 0 }
-                else if (quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
+                if (!(irregularitems.includes(item.name)) && item.quality > 0 ){
+                    item.quality -= 1 }
+                else if (!(item.name == "Aged Brie")){
+                    item.quality = 0 }
+                else if (item.quality < 50) {
+                    item.quality += 1
                     
                 }
             }
