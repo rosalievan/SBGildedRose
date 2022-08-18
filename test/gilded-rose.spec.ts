02271f747bd6bf5ea74eb81e051bf61994f33b1a
rosalievan <r.vanonzenoort98@gmail.com>
2022-08-18 14:38:33 +0100

    faulty code

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -7,14 +7,14 @@ describe('Gilded Rose', function () {
     it('normal item should decrease in quality', function() {
         const gildedRose = new GildedRose([ new Item('foo', 0, 25) ]);
         const items = gildedRose.updateQuality();
-        console.log(items);
+        // console.log(items);
         expect(items[0].quality).to.equal(23);
     });
 
     it('Backstage passes to a TAFKAL80ETC concert increases in quality', function (){
         const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC', 4, 25) ]);
         const items = gildedRose.updateQuality();
-        console.log(items);
+        // console.log(items);
         expect(items[0].quality).to.equal(28);
     });
 
